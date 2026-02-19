8a559cc11fa88124e37e395b6b4f949d71ed611a
Robin Wedin <robin.wedin@outlook.com>
2026-02-19 16:05:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define ELEVER 5 
+#define PROV 13 
+
+int main () {
+char namn
+int poäng
+double medel 
+double total
+
+for (int i = 0; i < ELEVER; i++) {
+scanf("%10s", namn [i]);
+ int sum = 0;
+
+
+
+
+
+  
+
