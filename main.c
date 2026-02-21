a8591e5df44a1f80c476c3454979b1f4d87947ab
Robin Wedin <robin.wedin@outlook.com>
2026-02-21 14:52:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,9 +10,21 @@ int poäng
 double medel 
 double total
 
-for (int i = 0; i < ELEVER; i++) {
+for (int i = 0; i < ELEVER; i++) 
+{
 scanf("%10s", namn [i]);
  int sum = 0;
+
+for (int j = 0; j < PROV; j++) 
+{ scanf("%d" , &poäng[i][j]);
+sum += poäng [i][j];
+}
+medel[i] = (double)sum / PROV;
+total += medel[i];
+
+namn [i][0] = toupper(namn[i][0]);
+for(int k = 1; namn[i][k] != '\0'; k++)
+{ 
 
 
 
