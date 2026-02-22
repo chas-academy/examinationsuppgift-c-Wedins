3784f9b89df2c03ca13cbc274a77583a6d4955b4
Robin Wedin <robin.wedin@outlook.com>
2026-02-22 17:36:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <ctype.h>
 
 #define ELEVER 5 
 #define PROV 13 
+struct Elev {
+char namn [11];
+double medel; 
+}
 
-int main () {
-char namn
-int poäng
-double medel 
-double total
-
+int main () 
+{
 for (int i = 0; i < ELEVER; i++) 
 {
-scanf("%10s", namn [i]);
- int sum = 0;
+int sum = 0;
+scanf("%10s", elever[i].namn)
+
+elever.namn[0]
+toupper(elever[i].namn[0]);
+ 
 
 for (int j = 0; j < PROV; j++) 
 { scanf("%d" , &poäng[i][j]);
-sum += poäng [i][j];
+sum = poäng [i][j];
 }
 medel[i] = (double)sum / PROV;
 total += medel[i];
